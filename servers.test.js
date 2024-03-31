8ba3d9bb8cf7cf2bb5c08d6090ed828dc172a384
Ryan Kerrigan <ryan.kerrigan22@gmail.com>
2024-03-31 18:00:50 -0500

    jasmine testing

--- servers.test.js
+++ servers.test.js
@@ -1,17 +1,5 @@
-// GOAL IS TO THOROUGHLY TEST EACH FUNCTION IN THE APP
-// STEP 1: READ CODE BASE, CLEAN UP THE DOM AFTER THE TEST IS RUN USING 'AFTEREACH', & WRITE A TEST FOR EACH FUNCTION FOUND IN SERVER.JS
-// STEP 2: CREATE HELPERS.TEST.JS AND TEST EACH FUNCTION IN HELPERS.JS & CREATE PAYMENTS.TEST.JS AND TEST EACH FUNCTION IN PAYMENTS.JS
-// STEP 3: TEST OWN CODE -- REVIEW FUNCTIONALITY OF appendTd(tr, value) -- CREATE A appendDeleteBtn(tr), WILL BE SIMILAR TO append(tr, value) 
-// WILL CREATE A 'TD' WITH THE VALUE X WHEN CLICKED IT WILL DELETE THE TABLE ROW IT BELONGS TO. --
-// -- WRITE THE FUNCTIONALITY FOR APPENDING A 'TD' TO A 'TR' WITH THE VALUE X -- SET AN EVENT LISTENER ON THE 'TD' THAT WILL REMOVE THE PARENT
-// 'TR' FROM THE DOM. YOU WILL HAVE TO FIND A WAY TO ACCESS THE PARENT ROW OF THE 'TD' FROM THE CLICK EVENT --
-// WRITE TESTS FOR appendDeleteBtn(tr) -- DONT SPEND TIME TESTING HTML AFTER DOM IS UPDATED DIFFICULTY SIMULATING A CLICK WITH VANILLA JS --
-// REPEAT THE PROCESS FOR REMOVING A PAYMENT FROM THE PAYMENT TABLE
-
-
 describe("Servers test (with setup and tear-down)", function() {
   beforeEach(function () {
-    // initialization logic
     serverNameInput.value = 'Alice';
   });
 
@@ -22,7 +10,28 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should not add a new server on submitServerInfo() with empty input', function () {
+    serverNameInput.value = '';
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+  it('should update #servertable on updateServerTable()', function () {
+    submitServerInfo();
+    updateServerTable();
+
+    let curTdList = document.querySelectorAll('#serverTable tbody tr td');
+
+    expect(curTdList.length).toEqual(3);
+    expect(curTdList[0].innerText).toEqual('Alice');
+    expect(curTdList[1].innerText).toEqual('$0.00');
+    expect(curTdList[2].innerText).toEqual('X');
+  });
+
   afterEach(function() {
-    // teardown logic
+    serverId = 0;
+    serverTbody.innerHTML = '';
+    allServers = {};
   });
 });
